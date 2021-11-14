591f8255567cb8c1fb5559ba0d030db7220696b9
lluey <lluey@students.ubc.ca>
2021-11-14 15:09:05 -0800

    commit all

--- Screens/LetterSelection.js
+++ Screens/LetterSelection.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
-
 const LetterSelection = () => {
   return (
     <View style={styles.container}>
