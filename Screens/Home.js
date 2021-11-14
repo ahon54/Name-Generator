7e7ad73c914525f48e19c6bad0c24f64449f4c89
lluey <lluey@students.ubc.ca>
2021-11-14 13:48:48 -0800

    style changes to home buttons

--- Screens/Home.js
+++ Screens/Home.js
@@ -5,10 +5,13 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   background-color: #5BCFFB;
-  color: black;
+  color: white;
   font-size: 20px;
-  padding: 10px 60px;
-  margin: 10px 0px;
+  padding: 40px 60px;
+  border-radius: 15px;
+  margin: 40px 0px;
+  border: none;
+  font-weight: bold;
 `
 const Home = () => {
   const handleMtoF = () => {
