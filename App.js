8acd32cf397cf6e350b739cd37c35c3a0ad55ec5
Adrian Hon <ahon2798@gmail.com>
2021-11-14 10:15:38 -0800

    components boiler plate

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
