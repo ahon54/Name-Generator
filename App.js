714c6ca677220229814aca64fb0ab73fcef7524f
Adrian Hon <ahon2798@gmail.com>
2021-11-14 11:10:36 -0800

    main component pages set up

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-
 export default function App() {
   return (
     <View style={styles.container}>
