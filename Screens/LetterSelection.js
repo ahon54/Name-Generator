1d0931e13020bf1c1f1b6901d04db37dd6713fca
Adrian Hon <ahon2798@gmail.com>
2021-11-14 11:54:50 -0800

    finished with navigation and set up components

---
+++ Screens/LetterSelection.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+const LetterSelection = () => {
+  return (
+    <View style={styles.container}>
+      <Text>LetterSelection</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default LetterSelection;
